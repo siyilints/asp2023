5463e1e2fb2aeeca9dea2f04f766d65bb0d86682
siyilints <siyili.nts@cstcollege.ca>
2023-02-03 13:44:51 -0500

    first commit

---
+++ Day3/CstDay3/CstSwitchApp/CstSwitchApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CstSwitchApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Print out student mark according
+             * Score
+             */
+
+            Console.WriteLine("please input student's score (integer between 0~100)");
+
+            int points = Convert.ToInt32(Console.ReadLine());
+            #region
+            // start switch
+            switch (points / 10)
+            {
+                case 10:
+                    Console.WriteLine("great");
+                    break;
+                case 9:
+                    Console.WriteLine("perfect");
+                    break;
+                case 8:
+                    Console.WriteLine("good");
+                    break;
+                case 7:
+                    Console.WriteLine("pass");
+                    break;
+                case 6:
+                    Console.WriteLine("pass");
+                    break;
+                default:
+                    Console.WriteLine("failed");
+                    break;
+            }
+            // end switch
+            #endregion
+
+
+        }
+    }
+}
