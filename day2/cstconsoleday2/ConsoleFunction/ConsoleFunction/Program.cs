5f12a281d9fb58ed713e74c7c1f01b22a9d77645
siyilints <siyili.nts@cstcollege.ca>
2023-02-03 09:32:43 -0500

    first commit

---
+++ day2/cstconsoleday2/ConsoleFunction/ConsoleFunction/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SayHello();
+
+            //void SayHello()
+            //{
+            //  Console.WriteLine("Welcome to CST College.!!");
+
+            //  Console.ReadKey();}
+
+            string mystr = "Tom";
+            string resultstr;
+            resultstr = SayHello(mystr);
+            Console.WriteLine(resultstr);
+            Console.ReadLine();
+
+            string SayHello(string str)
+            {
+                string message = "Welcome to CST College:" + str;
+                return message;
+
+
+            }
+
+
+        }
+
+
+    }
+}
